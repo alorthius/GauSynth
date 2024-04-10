1dd32b3190d429a00e405a50d7c7d46c4923ce0a
Anna-Alina Bondarets <anbondaret@softserveinc.com>
2024-04-10 22:22:46 +0300

    update inference

--- fooocus_inference.py
+++ fooocus_inference.py
@@ -10,21 +10,22 @@ from utils import *
 host = "http://127.0.0.1:8888"
 
 
-def text2img(params: dict) -> dict:
-    """
-    text to image
-    """
-    result = requests.post(url=f"{host}/v1/generation/text-to-image",
-                           data=json.dumps(params),
-                           headers={"Content-Type": "application/json"})
-    return result.json()
+# def text2img(params: dict) -> dict:
+#     """
+#     text to image
+#     """
+#     result = requests.post(url=f"{host}/v1/generation/text-to-image",
+#                            data=json.dumps(params),
+#                            headers={"Content-Type": "application/json"})
+#     return result.json()
 
 
 def image_prompt(params: dict) -> dict:
     """
     image prompt
     """
-    response = requests.post(url=f"{host}/v2/generation/image-prompt",
+    # response = requests.post(url=f"{host}/v2/generation/image-prompt",
+    response = requests.post(url=f"{host}/v2/generation/image-upscale-vary",
                              data=json.dumps(params),
                              headers={"Content-Type": "application/json"})
     return response.json()
@@ -37,30 +38,56 @@ def image_prompt(params: dict) -> dict:
 # print(result)
 
 
-image = open("silk.jpg", "rb").read()
+image = open("test_input/silk2k.jpg", "rb").read()
+liquid = open("test_input/liquid.png", "rb").read()
+
+advance_params = {
+    "overwrite_vary_strength": 0.7,
+    "mixing_image_prompt_and_vary_upscale": False,
+    # "canny_low_threshold": 250,
+    # "canny_high_threshold": 255,
+}
 
 params = {
-    "prompt": "Blue silk texture",
+    "performance_selection": "Quality",
     "aspect_ratios_selection": "1024*1024",
+    "image_seed": 666,
+    "sharpness": 2.0,  # 0-30
+    "guidance_scale": 4.0,  # 1-30
+    "base_model_name": "juggernautXL_v9Rundiffusion.safetensors",
+    "style_selections": ["Fooocus V2", "Fooocus Enhance", "Fooocus Sharp"],
+
+    "prompt": "Blue silk texture",
+
     "require_base64": True,
     "image_prompts": [
         {
-            "cn_img": base64.b64encode(image).decode('utf-8'),
-            "cn_stop": 0.6,
-            "cn_weight": 0.6,
+            "cn_img": base64.b64encode(liquid).decode('utf-8'),
+            "cn_stop": 0.8,
+            "cn_weight": 1,
             "cn_type": "ImagePrompt"
-        },{
-            "cn_img": base64.b64encode(image).decode('utf-8'),
+        }, {
+            "cn_img": base64.b64encode(liquid).decode('utf-8'),
             "cn_stop": 0.6,
-            "cn_weight": 0.6,
-            "cn_type": "ImagePrompt"
+            "cn_weight": 1,
+            "cn_type": "PyraCanny"
         }],
     "async_process": False,
+
+
+    "advanced_params": advance_params,
+    "uov_method": "Vary (Subtle)",
+    "input_image": base64.b64encode(image).decode('utf-8'),
     }
 
-image = image_prompt(params)[0]["base64"]
-image = base64_to_opencv(image)
-cv2.imwrite("out.png", image)
+###### important!!!
+# if image_prompts are set in vary, mixing = True always!
+
+res = image_prompt(params)[0]
+print(res["finish_reason"])
+
+image = base64_to_opencv(res["base64"])
+cv2.imwrite("test_output/out.png", image)
 
 
 # print(json.dumps(result, indent=4, ensure_ascii=False))
