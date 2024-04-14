ba6830c6dcc79049fc304e52930bf635a58ce3d5
alorthius <anna-alina.bondarets@ucu.edu.ua>
2024-04-14 15:12:09 +0300

    update video merging

--- scripts/process_video.py
+++ scripts/process_video.py
@@ -6,4 +6,4 @@ def split_video(fps, vid_path, output_path, resolution):
 
 
 def form_video(fps, frames_path, output_vid):
-    os.system(f"ffmpeg -framerate {fps} -pattern_type glob -i '{frames_path}/*.png' -c:v libx264 -pix_fmt yuv420p -vf 'pad=ceil(iw/2)*2:ceil(ih/2)*2' {output_vid}")
+    os.system(f"ffmpeg -y -framerate {fps} -pattern_type glob -i '{frames_path}/*.png' -c:v libx264 -pix_fmt yuv420p -vf 'pad=ceil(iw/2)*2:ceil(ih/2)*2' {output_vid}")
